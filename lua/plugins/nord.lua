d17825fe55a12b68eb3837770e71026bbd706a1b
Henrik Romnes <hbr@synergysky.com>
2025-02-19 11:14:14 +0000

    lock.json

--- lua/plugins/nord.lua
+++ lua/plugins/nord.lua
@@ -2,7 +2,7 @@ return {
     "shaunsingh/nord.nvim",
     name = "nord.nvim",
     config = function()
-        --vim.cmd("colorscheme nord")
+        -- vim.cmd("colorscheme nord")
         vim.g.nord_contrast = true
         vim.g.nord_cursorline_transparent = true
     end
