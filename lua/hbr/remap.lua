9eccb6a71baaba329d503f9f281dab5fd07f1a89
Henrik Romnes <hbr@synergysky.com>
2025-05-04 08:50:09 +0200

    snipe delete

--- lua/hbr/remap.lua
+++ lua/hbr/remap.lua
@@ -69,4 +69,7 @@ vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+-- Keep cursor at original position when deleting
+vim.keymap.set('n', 'di"', 'mzdi"`z', { noremap = true, silent = true })
+vim.keymap.set('n', 'da"', 'mzda"`z', { noremap = true, silent = true })
 
